80b0621e09376076dd37546cc33a60969ceb6d4d
Steve Kieu <steve.kieu@xvt.com.au>
2018-02-23 19:20:27 +1000

    Keep last kernel

--- porteus-install-kernel.sh
+++ porteus-install-kernel.sh
@@ -6,13 +6,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CURRENT_KVER=$(uname -r)
 
 # Clean up
-LAST_FILE=$(find $PORT_DIR/ -maxdepth 1 -type f -name "000*linux-header*.xzm" -printf "%T+\t%p\n" | grep -v  "*${CURRENT_KVER}*.xzm" | sort -r | head -n 1 | awk '{print $2}')
+LAST_FILE=$(find $PORT_DIR/ -maxdepth 1 -type f -name "000*linux-header*.xzm" -printf "%T+\t%p\n" | grep -v  "*${CURRENT_KVER}*.xzm" | sort -r )
 
-find $PORT_DIR/ -maxdepth 1 -type f -name "000*linux-header*.xzm" | grep -v  "*${CURRENT_KVER}*.xzm" | grep -v "$LAST_FILE" | while read fn; do rm -f "$fn"; done
+LINE_COUNT=$(echo "$LAST_FILE" | wc -l )
 
-LAST_FILE=$(find $PORT_DIR/ -maxdepth 1 -type f -name "000*.xzm" -printf "%T+\t%p\n" | grep -v  "*${CURRENT_KVER}*.xzm" | sort -r | head -n 1 | awk '{print $2}')
+if [ $LINE_COUNT -gt 2 ]; then
+    echo "$LAST_FILE" | head -n 1 | awk '{print $2}' |  while read fn; do rm -f "$fn"; done
+fi
 
-find $PORT_DIR/ -maxdepth 1 -type f -name "000*.xzm" | grep -v  "*${CURRENT_KVER}*.xzm" | grep -v "$LAST_FILE" | while read fn; do rm -f "$fn"; done
+LAST_FILE=$(find $PORT_DIR/ -maxdepth 1 -type f -name "000*.xzm" -printf "%T+\t%p\n" | grep -v  "*${CURRENT_KVER}*.xzm" | grep -v "linux-header" | sort -r )
+LINE_COUNT=$(echo "$LAST_FILE" | wc -l )
+if [ $LINE_COUNT -gt 2 ]; then
+    echo "$LAST_FILE" | head -n 1 | awk '{print $2}' |  while read fn; do rm -f "$fn"; done
+fi
 
 # Copy new kernel
 
