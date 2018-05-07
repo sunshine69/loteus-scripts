700036874dcd688e4f3e1edb77ebc8116a676cee
steve kieu <stevek@stevek-macbook.vm>
2018-05-07 11:23:08 +1000

    Allow multiple kernel modules copy into initrd

--- rebuild-initrd.sh
+++ rebuild-initrd.sh
@@ -19,31 +19,41 @@ fi
 
 [ ! -f "$INF" ] && echo "Input file $INF does not exist" && exit 1
 
-mkdir /tmp/initrd_$$ && cd /tmp/initrd_$$
+mkdir /tmp/initrd_$$
+cd /tmp/initrd_$$
 
 xzcat $INF | cpio -id
 cp -a $SCRIPT_DIR/linuxrc .
-echo "Copy modules in - /tmp/initrd_$$/lib/modules/ if needed and then type kernel version in"
-echo "Enter kernel version: "
-read KVER
 
-if [ ! -z "$KVER" ]; then
-    DESTDIR="/tmp/initrd_$$/lib/modules/$KVER" ; rm -rf $DESTDIR; mkdir -p $DESTDIR
-    if [ -z "$SRCDIR" ]; then
-        if [ -z "$KBUILDDIR" ]; then
-            # Install xzm modules
-            $TARGET_DIR/porteus-kernel-$KVER.tar.sfx
-            KBUILDDIR="$PORT_DIR"
+echo "Copy modules in - /tmp/initrd_$$/lib/modules/ if needed and then type kernel version in"
+
+echo "Enter kernel version (space separated for a list): "
+read KVERS
+
+if [ ! -z "$KVERS" ]; then
+    for KVER in $KVERS; do
+        KBUILDDIR=""
+        SRCDIR=""
+        DESTDIR="/tmp/initrd_$$/lib/modules/$KVER" ; rm -rf $DESTDIR; mkdir -p $DESTDIR
+        if [ -z "$KMOD_DIR" ]; then
+            if [ -z "$KBUILDDIR" ]; then
+                #Install xzm modules
+                ( cd $CWD ; $TARGET_DIR/porteus-kernel-$KVER.tar.sfx )
+                KBUILDDIR="$CWD/porteus-kernel"
+            fi
+            mkdir $KBUILDDIR/1 ; mount -o loop $KBUILDDIR/000-$KVER.xzm $KBUILDDIR/1
+            SRCDIR="$KBUILDDIR/1/lib/modules/$KVER/kernel"
+        else
+            SRCDIR=$KMOD_DIR
         fi
-        mkdir $KBUILDDIR/1 ; mount -o loop $KBUILDDIR/000-$KVER.xzm $KBUILDDIR/1
-        SRCDIR="$KBUILDDIR/1/lib/modules/$KVER/kernel"
-    fi
-    cp -a $SRCDIR/{crypto,lib} $DESTDIR/
-    cp -a $SRCDIR/drivers/{hid,ata,block,acpi,crypto,md,memstick,mmc} $DESTDIR/
-    cp -a $SRCDIR/drivers/hwmon/applesmc.ko $SRCDIR/drivers/input/input-polldev.ko $DESTDIR/
-    cp -a $SRCDIR/fs/{jfs,reiserfs,xfs,aufs,btrfs,f2fs,fat,isofs,nls,overlayfs,udf,ufs,binfmt_misc.ko} $DESTDIR/
-    depmod $KVER -b .
-    umount $KBUILDDIR/1; rm -rf $KBUILDDIR/1
+        cp -a $SRCDIR/{crypto,lib} $DESTDIR/
+        cp -a $SRCDIR/drivers/{hid,ata,block,acpi,crypto,md,memstick,mmc} $DESTDIR/
+        cp -a $SRCDIR/drivers/hwmon/applesmc.ko $SRCDIR/drivers/input/input-polldev.ko $DESTDIR/
+        cp -a $SRCDIR/fs/{jfs,reiserfs,xfs,aufs,btrfs,f2fs,fat,isofs,nls,overlayfs,udf,ufs,binfmt_misc.ko} $DESTDIR/
+        depmod $KVER -b .
+        umount $KBUILDDIR/1; rm -rf $KBUILDDIR/1
+        if [ -d "$CWD/porteus-kernel" ]; then rm -rf "$CWD/porteus-kernel"; fi
+    done
 fi
 
 mc /tmp/initrd_$$/lib/modules/
