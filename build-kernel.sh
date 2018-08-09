c1d5a940305a3f3d52cea94b48bd4e1f05855966
Steve Kieu <steve.kieu@xvt.com.au>
2018-08-09 11:36:57 +1000

    Added build-kernel script

---
+++ build-kernel.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -ex
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+KSOURCE_DIR=/mnt/sda4/tmp
+
+LONGTERM=14
+STABLE=17
+
+VERSION=4
+PATCHLEVEL=14
+SUBLEVEL=$(grep -oP '(?<=SUBLEVEL \= )([\d]+)' linux-${VERSION}.${PATCHLEVEL}/Makefile)
+
+OLD_KVER="${VERSION}.${PATCHLEVEL}.${SUBLEVEL}"
+
+LOCAL_VER=$(grep -Po '(?<=CONFIG_LOCALVERSION=")([^"]+)' linux-${VERSION}.${PATCHLEVEL}/.config)
+
+KVER="$1"
+
+if [ -z "$KVER" ]; then
+    if [ "$PATCHLEVEL" == "$STABLE" ]; then
+        KVER=`curl -Ls http://www.kernel.org | python3 -c 'import sys; from bs4 import BeautifulSoup; s=BeautifulSoup(sys.stdin.read(), "html.parser"); print(s.body.find("td", attrs={"id":"latest_link"}).text.replace("\n","") )'`
+    elif [ "$PATCHLEVEL" == "$LONGTERM" ]
+    # Get the longterm
+        KVER=`curl -Ls http://www.kernel.org | python3 -c 'import sys; from bs4 import BeautifulSoup; s=BeautifulSoup(sys.stdin.read(), "html.parser"); print(s.body.find("td", text="longterm:").next_sibling.next_sibling.text)'`
+    fi
+fi
+
+if [ "$KVER" == "$OLD_KVER" ]; then echo "No new version. Do nothing"; exit 0; fi
+
+pushd .
+
+cd $KSOURCE_DIR
+
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/patch-$KVER.xz
+
+cd linux-${VERSION}.${PATCHLEVEL}
+
+if [ "$SUBLEVEL" != "0" ]; then
+    xzcat ../patch-${OLD_KVER}.xz | patch -p1 -R
+fi
+
+xzcat ../patch-${KVER}.xz | patch -p1
+
+if [ ! -z "$CONFIG_FILE" ]; then cp "$CONFIG_FILE" .config; fi
+
+make clean oldconfig
+make -j 8 bzImage modules
+
+# KVER="${VERSION}.${PATCHLEVEL}.${SUBLEVEL}"
+export KVERS="${KVER}${LOCAL_VER}"
+$SCRIPT_DIR/makekernel.sh xz
+
+popd
