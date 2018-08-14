8f1849187dca8d6d4f9f4c96fa4fc41095a34fd0
Steve Kieu <steve.kieu@xvt.com.au>
2018-08-14 21:18:36 +1000

    Update

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
+if [ "$(id -u)" != "0" ]; then
+    exec sudo $0
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 KSOURCE_DIR=/mnt/sda4/tmp
 
@@ -19,7 +23,7 @@ KVER="$1"
 if [ -z "$KVER" ]; then
     if [ "$PATCHLEVEL" == "$STABLE" ]; then
         KVER=`curl -Ls http://www.kernel.org | python3 -c 'import sys; from bs4 import BeautifulSoup; s=BeautifulSoup(sys.stdin.read(), "html.parser"); print(s.body.find("td", attrs={"id":"latest_link"}).text.replace("\n","") )'`
-    elif [ "$PATCHLEVEL" == "$LONGTERM" ]
+    elif [ "$PATCHLEVEL" == "$LONGTERM" ]; then
     # Get the longterm
         KVER=`curl -Ls http://www.kernel.org | python3 -c 'import sys; from bs4 import BeautifulSoup; s=BeautifulSoup(sys.stdin.read(), "html.parser"); print(s.body.find("td", text="longterm:").next_sibling.next_sibling.text)'`
     fi
