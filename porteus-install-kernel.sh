413497f6c42f4f1921778d38bc7c362e6b168104
Steve Kieu <steve.kieu@xvt.com.au>
2019-08-13 09:41:59 +1000

    Put some debugging method to see sometimes it removes things unexpectedly.

--- porteus-install-kernel.sh
+++ porteus-install-kernel.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPT_DIR/common.sh
 
+CURRENT_DIR=$(pwd)
+
 CURRENT_KVER=$(uname -r)
 
 # Clean up
@@ -17,7 +19,7 @@ fi
 LAST_FILE=$(find $PORT_DIR/ -maxdepth 1 -type f -name "000*.xzm" -printf "%T+\t%p\n" | grep -v  "*${CURRENT_KVER}*.xzm" | grep -v "linux-header" | sort -r )
 LINE_COUNT=$(echo "$LAST_FILE" | wc -l )
 if [ $LINE_COUNT -gt 2 ]; then
-    echo "$LAST_FILE" | head -n 1 | awk '{print $2}' |  while read fn; do rm -f "$fn"; done
+    echo "$LAST_FILE" | head -n 1 | awk '{print $2}' |  while read fn; do echo "Going to rm '$fn'"; rm -f "$fn"; done
 fi
 
 # Copy new kernel
@@ -26,5 +28,7 @@ mv $BOOT_DIR/bzImage $BOOT_DIR/bzImage.old
 mv $BOOT_DIR/initrd.xz $BOOT_DIR/initrd.xz.old
 mv initrd.xz bzImage $BOOT_DIR/
 mv 000-*.xzm $PORT_DIR/
-cd ..
+
+cd $CURRENT_DIR
+echo "Going to remove '$SCRIPT_DIR'"
 rm -rf "$SCRIPT_DIR"
