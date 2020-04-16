3b5c102ebc28e24590f7702bae8be7c567610ec6
Steve Kieu <steve.kieu@xvt.com.au>
2020-04-16 10:36:01 +1000

    IMprovement re-pack

--- re-pack.sh
+++ re-pack.sh
@@ -3,6 +3,10 @@
 # Used to re-pack the current kernel mod dir into
 # When build a new kernel / reboot and we can run this to recompile external modules and re-pack
 
+# Usage: $0 <path_to_kernel_mod_file> <path_to_kernel_source_xzm_mod_file>
+
+# Argument are options, if not provided it will use current running kernel and default kernel source dir in the symlink `source` in /lib/modules/<kver>/source
+
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export KBIN_DIR=/mnt/sda4/build/kernel-binary
 export WORK_DIR=/mnt/sda4/tmp
@@ -18,19 +22,36 @@ if [ "$KVER" = "" ]; then
     KMOD_MOUNT="/mnt/live/memory/images/000-${KVER}.xzm"
 else
     NOT_RUNNING_KERNEL="y"
-    mount -o loop $KVER repack1
+    [ ! -f $KVER ] && echo "Kernel mod file does not exist, aborting ..." && exit 1
+    LOOP_DEV=$(losetup -f)
+    losetup $LOOP_DEV $KVER
+    mount $LOOP_DEV repack1
     KVER=$(ls repack1/lib/modules/)
     KMOD_MOUNT="${WORK_DIR}/repack1"
     if [ "$2" = "" ]; then
         KSOURCE_DIR="source"
     else
-        mount -o loop "$2" repack1s
+        if [ ! -f "$2" ]; then
+            echo "Kernel source mod file does not exists, aborting ..."
+            losetup -d $LOOP_DEV >/dev/null 2>&1
+            exit 1
+        fi
+        LOOP_DEV_S=$(losetup -f); losetup $LOOP_DEV_S $2
+        mount $LOOP_DEV_S repack1s
         mount -t overlay overlay -o lowerdir=repack1s,upperdir=repack2s,workdir=repack-wds repack3s
         KSOURCE_DIR="${WORK_DIR}/repack3s"
     fi
 fi
 
 mount -t overlay overlay -o lowerdir=${KMOD_MOUNT},upperdir=repack2,workdir=repack-wd repack3
+(cd repack3/lib/modules/${KVER}; ln -sf $KSOURCE_DIR build)
+
+echo "KSOURCE_DIR: $KSOURCE_DIR Mod read-write mount: $WORK_DIR/repack3/lib/modules/${KVER}"
+
+if [ -d /lib/modules/${KVER} ]; then
+    mount -o bind $WORK_DIR/repack3/lib/modules/${KVER} /lib/modules/${KVER}
+#   (cd /lib/modules/${KVER} ; ln -sf source build )
+fi
 
 if [ "$NOT_RUNNING_KERNEL" = "y" ]; then
     echo "Building for different kernel than the current running kernel"
@@ -38,13 +59,10 @@ if [ "$NOT_RUNNING_KERNEL" = "y" ]; then
     echo "Kernel source dir is at '$KSOURCE_DIR'"
     echo "Use these dir to manually make your kernel modules. When done, hit enter."
     read _junk
-else
-    mount -o bind repack3/lib/modules/${KVER} /lib/modules/${KVER}
-    (cd /lib/modules/${KVER} ; ln -sf source build )
 fi
 
 echo ""
-echo "Make module read write, now go and compile external modules as required. When done hit enter"
+echo "Make module read write, now go and compile external modules as required\nYou may need to populate the source When done hit enter"
 read _junk
 
 mksquashfs repack3 000-${KVER}.xzm -comp xz -b 1M
@@ -76,6 +94,10 @@ rm -rf porteus-kernel porteus-kernel-${KVER}.tar
 umount /lib/modules/${KVER} >/dev/null 2>&1
 umount repack3 repack3s  >/dev/null 2>&1
 umount repack1  >/dev/null 2>&1
+umount repack1s  >/dev/null 2>&1
+losetup -d $LOOP_DEV >/dev/null 2>&1
+losetup -d $LOOP_DEV_S >/dev/null 2>&1
+
 rm -rf repack1 repack2 repack3 repack1s repack2s repack3s repack-wds repack-wd
 
 popd
