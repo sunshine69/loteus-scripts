9aadae5dc1bb29eb6a0416f66c046b502cb3f360
Steve Kieu <steve.kieu@xvt.com.au>
2020-04-24 15:09:48 +1000

    Updates
    - fix bug to make dynamically make changes dir to work
    - update update-image.sh

--- porteus-scripts/update-image.sh
+++ porteus-scripts/update-image.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 SRC=$1
-ENC=$2
-if [ "$3" ]; then WORKDIR="$3"; else WORKDIR=/mnt/live; fi
+
+[ -z "$SRC" ] && echo "Usage: $0 <image_file> [work_dir|pwd] [enc] [enc password]" && exit 1
+
+WORKDIR=$2
+
+[ -z "$WORKDIR" ] && WORKDIR=$(pwd)
+
+ENC=$3
 PASS=$4
 
 NAME_PREFIX='update_mod'
 
-CDIR=`pwd`; cd $WORKDIR; rm -rf ${NAME_PREFIX}1 ${NAME_PREFIX}2 ${NAME_PREFIX}3 >/dev/null 2>&1; mkdir ${NAME_PREFIX}1 ${NAME_PREFIX}2 ${NAME_PREFIX}3 >/dev/null 2>&1
-SRCPATH="`realpath $1`"
+CDIR=`pwd`; cd $WORKDIR; rm -rf ${NAME_PREFIX}1 ${NAME_PREFIX}2 ${NAME_PREFIX}3 ${NAME_PREFIX}-wd >/dev/null 2>&1; mkdir ${NAME_PREFIX}1 ${NAME_PREFIX}2 ${NAME_PREFIX}3 ${NAME_PREFIX}-wd >/dev/null 2>&1
+
+SRCPATH="`realpath $SRC`"
 
 LODEV=`losetup -f`; losetup $LODEV $SRCPATH
+
 if [ $? == 0 ]; then
 	if blkid $LODEV 2>/dev/null | cut -d" " -f3- | grep -q _LUKS; then
 		if [ -z $PASS ]; then read -s -p "Enter pass:" PASS; fi
@@ -24,32 +32,68 @@ else
   echo "Error mount old changes module. New changes will be saved into new module"
   LODEV=''
 fi
-mount -t aufs none ${NAME_PREFIX}3 -o br=${NAME_PREFIX}2=rw:${NAME_PREFIX}1=ro
-if [ $? != 0 ]; then echo "Fatal Error mount aufs"; umount ${NAME_PREFIX}1; losetup -d $LODEV >/dev/null 2>&1; exit 1;fi
 
-echo "Mount done, start subshell now on hte mount point. Copy and modify files under it."
+#mount -t aufs none ${NAME_PREFIX}3 -o br=${NAME_PREFIX}2=rw:${NAME_PREFIX}1=ro
+mount -t overlay overlay -o lowerdir=${NAME_PREFIX}1,upperdir=${NAME_PREFIX}2,workdir=${NAME_PREFIX}-wd ${NAME_PREFIX}3
+
+
+if [ $? != 0 ]; then echo "Fatal Error mount layered fs"; umount ${NAME_PREFIX}1; losetup -d $LODEV >/dev/null 2>&1; exit 1;fi
+
+echo "Mount done, start subshell now on the mount point. Copy and modify files under it."
+
 echo "When done, type exit to exit this shell and I will continue "
+
 ( cd ${NAME_PREFIX}3 && /bin/bash )
 
 cd $WORKDIR
 rm -f out.sqs
 if [ -z "$SQUASHFS_OPT" ]; then
-	SQUASHFS_OPT="-comp xz -b 1024K"
-	echo "Default SQUASHFS_OPT is  '-comp xz -b 1024K'"
+	# Best balance now seems to be lz4 -Xhc. The zstd is good to built rescue but level 19 is too slow
+	SQUASHFS_OPT="-comp zstd -b 1024K -Xcompression-level 15"
+	#SQUASHFS_OPT="-comp lz4 -Xhc -b 1024K"
+	#SQUASHFS_OPT="-b 1024K"
+	echo "Default SQUASHFS_OPT is '$SQUASHFS_OPT' - fast enough and good compression"
+	echo "0. -comp lz4 -Xhc -b 1024K - Fast to compress/decompress, size is bugger the zstd. Small memory"
+	echo "1. -comp zstd -b 1024K -Xcompression-level 19 - good compress but compress slow"
+	echo "2. -comp gzip -b 1M - bigger size than zstd"
+	echo "3. -comp xz -b 1M - compress best but slowest"
+	echo "Enter your selection as number or your own option string. Hit enter to choose default"
+	read ans
+	case "$ans" in
+		0)
+			SQUASHFS_OPT="-comp lz4 -Xhc -b 1024K";
+			;;
+		1)
+			SQUASHFS_OPT="-comp zstd -b 1024K -Xcompression-level 19";
+			;;
+		2)
+			SQUASHFS_OPT="-comp gzip -b 1M";
+			;;
+		3)
+			SQUASHFS_OPT="-comp xz -b 1M";
+			;;
+		*)
+			if [ ! -z "$ans" ]; then SQUASHFS_OPT="$ans"; fi
+			echo "Use '$SQUASHFS_OPT'"
+	esac
 fi
+
 OUTDIR=${NAME_PREFIX}3
 mksquashfs $OUTDIR out.sqs $SQUASHFS_OPT
+
 umount ${NAME_PREFIX}3; sleep 3; umount ${NAME_PREFIX}1 >/dev/null 2>&1
+
 if `ls /dev/mapper/${NAME_PREFIX}_DEC >/dev/null 2>&1`; then
 	cryptsetup luksClose /dev/mapper/${NAME_PREFIX}_DEC
 fi
+
 if [ "$LODEV" ]; then losetup -d $LODEV; fi
-if [ "$2" == 'enc' ]; then
+if [ "$ENC" == 'enc' ]; then
 	/opt/bin/squash2enc.sh out.sqs $SRCPATH $PASS # Given the existing module so we will use the existing luks
 	mv out.sqs.enc out.sqs
 fi
 echo "run 'mv $WORKDIR/out.sqs ${SRCPATH}.new' ? y/n"
 read a
 if [ "$a" == 'y' ]; then mv $WORKDIR/out.sqs ${SRCPATH}.new; else echo "Output file is $WORKDIR/out.sqs";fi
-rm -rf ${NAME_PREFIX}1 ${NAME_PREFIX}2 ${NAME_PREFIX}3 ${NAME_PREFIX}ENC ${NAME_PREFIX}D >/dev/null 2>&1
+rm -rf ${NAME_PREFIX}1 ${NAME_PREFIX}2 ${NAME_PREFIX}3 ${NAME_PREFIX}-wd ${NAME_PREFIX}ENC ${NAME_PREFIX}D >/dev/null 2>&1
 cd $CDIR
