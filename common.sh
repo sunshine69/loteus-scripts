79aa3a4b4adcde38174b90a729eabf6b51c23927
Steve Kieu <msh.computing@gmail.com>
2021-05-14 22:31:01 +1000

    Improve dir detection

--- common.sh
+++ common.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-[ -z "$TARGET_DIR" ] && TARGET_DIR=/mnt/portdata/build/kernel-binary
 
 BZIMAGE_PATH=$(cat /proc/cmdline | grep -oP '(?<=BOOT_IMAGE=)[^\s]+(?=.*)')
 BZIMAGE_NAME=$(basename $BZIMAGE_PATH)
@@ -9,14 +8,16 @@ BZIMAGE_DIR=$(dirname $BZIMAGE_PATH)
 if ! `touch ${BZIMAGE_DIR}/test-file >/dev/null 2>&1`; then
     echo "${BZIMAGE_DIR} is not writable. Looks like we boot from cdrom. Will use hardcoded path from portdata"
     export BZIMAGE_FULL_PATH=/mnt/sr0/${BZIMAGE_NAME}
-    export PORT_DIR=/mnt/portdata/port
-    export BOOT_DIR=/mnt/portdata/boot
+    #export PORT_DIR=/mnt/portdata/port
+    #export BOOT_DIR=/mnt/portdata/boot
 else
     export BZIMAGE_FULL_PATH=$(find /mnt/sd*/${BZIMAGE_DIR}/ -maxdepth 2 -type f -name ${BZIMAGE_NAME})
-    export PORT_DIR=$(dirname `losetup -a|grep -P '000\-[\d\.]+'|awk '{print $3}'|cut -d'(' -f2` | head -n1)
-    export BOOT_DIR=$(dirname $BZIMAGE_FULL_PATH)
 fi
+export PORT_DIR=$(dirname `losetup -a|grep -P '000\-[\d\.]+'|awk '{print $3}'|cut -d'(' -f2` | head -n1)
+export BOOT_DIR=$(dirname $BZIMAGE_FULL_PATH)
 
 [ -z "$PROMPT" ] && PROMPT=y
+
+[ -z "$TARGET_DIR" ] && TARGET_DIR=$(dirname $PORT_DIR)/build/kernel-binary
 
 echo "TARGET_DIR=$TARGET_DIR PORT_DIR $PORT_DIR BOOT_DIR $BOOT_DIR"
