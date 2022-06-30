2248c3c13971af1d7ff5a7b0106aeef46f15310b
Steve Kieu <steve.kieu@ladbrokes.com.au>
2022-06-30 19:43:58 +1000

    UD

--- notify-update.py
+++ notify-update.py
@@ -24,28 +24,37 @@ version_info = [x.find('title').text for x in s.find_all('item')]
 # 'next-20220629: linux-next']
 
 version_info1 = [ list(map(str.strip, x.split(':'))) for x in version_info ]
-version_short_full_list = { '.'.join(x[0].split('.')[0:2]): x[0] for x in version_info1}
+lookup_full_ver_by_short_ver_dict = { '.'.join(x[0].split('.')[0:2]): x[0] for x in version_info1}
 
 short_ver_to_check_list = ['5.15','5.18']
 
-os.chdir('/mnt/portdata/tmp')
 ptn = re.compile(r'SUBLEVEL = ([\d]+)')
+portetus_kernel_filename_ptn = re.compile(r'porteus-kernel\-([\d]+\.[\d]+\.[\d]+)')
 
-output_ver_list = {}
+output_ver_list, local_full_ver = {}, ""
 
 for short_ver in short_ver_to_check_list:
-    text = open('linux-'+short_ver+'/Makefile', 'r').read()
-    m = ptn.search(text)
-    if m:
-        SUBLEVEL = m.group(1)
-        local_full_ver = short_ver + '.' + SUBLEVEL
-        if short_ver in version_short_full_list:
-            remote_current_ver = version_short_full_list[short_ver]
-            if version.parse(remote_current_ver) > version.parse(local_full_ver):
-                output_ver_list[short_ver] = {'local': local_full_ver, 'remote': remote_current_ver }
+    k_makefile = f'/mnt/portdata/tmp/linux-{short_ver}/Makefile'
+    if os.path.isfile(k_makefile):
+        text = open(k_makefile, 'r').read()
+        m = ptn.search(text)
+        if m:
+            SUBLEVEL = m.group(1)
+            local_full_ver = f'{short_ver}.{SUBLEVEL}'
+    else:
+        from glob import glob
+        kfile_list = glob(f'/mnt/sdb4/doc/opc-backup/porteus-kernel-{short_ver}*.sfx')
+        kfile_list.sort()
+        m = portetus_kernel_filename_ptn.search( kfile_list[-1] )
+        if m:
+            local_full_ver = m.group(1)
+
+    if short_ver in lookup_full_ver_by_short_ver_dict:
+        remote_current_ver = lookup_full_ver_by_short_ver_dict[short_ver]
+        if version.parse(remote_current_ver) > version.parse(local_full_ver):
+            output_ver_list[short_ver] = {'local': local_full_ver, 'remote': remote_current_ver }
 
 if len(output_ver_list) > 0:
     print('New version detected from remote')
     print(output_ver_list)
-    os.chdir('/home/stevek/src/kernel-build-scripts/porteus-scripts')
-    os.system('''./sendmail.py -f steve345@gmail.com -t msh.computing@gmail.com -s '{subject}' -m '{msg}' --server smtp.gmail.com -u skieu345@gmail.com -p "{passwd}"'''.format(subject='New linux kernel version detected from remote', msg=output_ver_list, passwd=os.getenv(MAIL_PASSWORD)) )
+    os.system('''sendmail.py -f steve345@gmail.com -t msh.computing@gmail.com -s '{subject}' -m '{msg}' --server smtp.gmail.com -u skieu345@gmail.com -p "{passwd}"'''.format(subject='New linux kernel version detected from remote', msg=output_ver_list, passwd=os.getenv('MAIL_PASSWORD')))
