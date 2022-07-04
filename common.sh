1f3f160ae1c46dd3f3066245d72990cd9c68aa7c
Steve Kieu <steve.kieu@ladbrokes.com.au>
2022-07-04 20:27:51 +1000

    ud

--- common.sh
+++ common.sh
@@ -5,13 +5,15 @@ BZIMAGE_PATH=$(cat /proc/cmdline | grep -oP '(?<=BOOT_IMAGE=)[^\s]+(?=.*)')
 BZIMAGE_NAME=$(basename $BZIMAGE_PATH)
 BZIMAGE_DIR=$(dirname $BZIMAGE_PATH)
 
-if ! `touch ${BZIMAGE_DIR}/test-file >/dev/null 2>&1`; then
-    echo "${BZIMAGE_DIR} is not writable. Looks like we boot from cdrom. Will use hardcoded path from portdata"
-    export BZIMAGE_FULL_PATH=/mnt/sr0/${BZIMAGE_NAME}
-    #export PORT_DIR=/mnt/portdata/port
-    #export BOOT_DIR=/mnt/portdata/boot
-else
-    export BZIMAGE_FULL_PATH=$(find /mnt/sd*/${BZIMAGE_DIR}/ -maxdepth 2 -type f -name ${BZIMAGE_NAME})
+if [ -z "$BZIMAGE_FULL_PATH" ]; then
+	if ! `touch ${BZIMAGE_DIR}/test-file >/dev/null 2>&1`; then
+	    echo "${BZIMAGE_DIR} is not writable. Looks like we boot from cdrom. Will use hardcoded path from portdata"
+	    export BZIMAGE_FULL_PATH=/mnt/sr0/${BZIMAGE_NAME}
+	    #export PORT_DIR=/mnt/portdata/port
+	    #export BOOT_DIR=/mnt/portdata/boot
+	else
+	    export BZIMAGE_FULL_PATH=$(find /mnt/nvme*/${BZIMAGE_DIR}/ /mnt/sd*/${BZIMAGE_DIR}/ -maxdepth 2 -type f -name ${BZIMAGE_NAME}|head -n1)
+	fi
 fi
 export PORT_DIR=$(dirname `losetup -a|grep -P '000\-[\d\.]+'|awk '{print $3}'|cut -d'(' -f2` | head -n1)
 export BOOT_DIR=$(dirname $BZIMAGE_FULL_PATH)
--- repack-porteus-kernel.sh
+++ repack-porteus-kernel.sh
@@ -18,6 +18,10 @@ if ! $(ls porteus-kernel/000-${KVER}* >/dev/null 2>&1); then
     exit 1
 fi
 rm -f porteus-kernel/*.bak
+find porteus-kernel/*.new | while read fn; do
+	_name=$(basename $fn .new)
+	mv $fn $(dirname $fn)/$_name
+done
 tar cf $TAR_FNAME porteus-kernel
 cat ${SCRIPT_DIR}/self-extract.sh $TAR_FNAME > $TARGET_FNAME
 rm -rf $TAR_FNAME porteus-kernel
