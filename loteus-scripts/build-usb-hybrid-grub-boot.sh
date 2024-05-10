01220065c6dae1b2e7a70cef14b07a2130489f93
Steve Kieu <msh.computing@gmail.com>
2024-05-11 09:35:44 +1000

    Update build boot disk

--- loteus-scripts/build-usb-hybrid-grub-boot.sh
+++ loteus-scripts/build-usb-hybrid-grub-boot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # This will install porteus into a disk given by $1 (LOOP_DEV)
 
@@ -11,11 +11,13 @@ if [ -z "$LOOP_DEV" ]; then
       - OS_DIR where to copy the OS dir base images, default to be the current running system OS dir.
       - CURRENT_BOOT_DIR - Path to the boot directory where the bzImage and initrd.xz will be installed. If not set it uses the current one.
       - FORCE_HYBRID value yes|no|legacy. Make the boot disk hybrid mode. By default if device is loop, then it is yes, otherwise is no (only EFI boot is setup). Set it to 'yes' to force building EFI and legacy hybrid disk.
-      If the value is 'legacy' then setup the boot disk is legacy only, no EFI."
+      If the value is 'legacy' then setup the boot disk is legacy only, no EFI.
+      - If the device is a disk partition then the partition will be formarted and the OS images will be installed on it. It will bypass the auto partition scheme and only copy files to that part"
     exit 1
 fi
 
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MKFS=${MKFS:-mkfs.btrfs -f}
 
 if [ ! -b "/dev/${LOOP_DEV}" ]; then
     echo "Not a block device /dev/${LOOP_DEV}"
@@ -23,12 +25,17 @@ if [ ! -b "/dev/${LOOP_DEV}" ]; then
 
 fi
 
-udevadm info --query=all /dev/${LOOP_DEV} | grep ID_USB_DRIVER
-if [ "$?" != "0" ] || [[ "$LOOP_DEV" == loop* ]]; then
+udevadm info --query=all /dev/${LOOP_DEV} > /tmp/dis_info_$$.txt
+if $(grep ID_USB_DRIVER /tmp/dis_info_$$.txt >/dev/null 2>&1) || [[ "$LOOP_DEV" == loop* ]]; then
     USB_BOOT_OPT="usb_delay=2"
+    IS_USB="true"
 else
     USB_BOOT_OPT=""
 fi
+
+DEVTYPE=$(grep -oP '(?<=DEVTYPE=)[^\s]+' /tmp/dis_info_$$.txt)
+DISK_DEVICE=$(lsblk --list --noheadings --output PKNAME /dev/${LOOP_DEV} | tail -n1)
+if [ -z "$DISK_DEVICE" ]; then DISK_DEVICE=${LOOP_DEV}; fi # lsblk cmd does not work well with loop dev;
 
 CURRENT_BOOT_FROM=$2
 CURRENT_BOOT_OS=$(grep -oP '(?<=os=)[^\s]+' /proc/cmdline)
@@ -87,95 +94,117 @@ umount /mnt/root/boot/efi || true
 umount /mnt/root || true
 umount /dev/${LOOP_DEV}* || true
 
-# partition disk
-# clear existing data
-sgdisk /dev/${LOOP_DEV} --zap-all
+set -e
 
-# create 1st partition
-sgdisk /dev/${LOOP_DEV} --new=1:0:+1M
-sgdisk /dev/${LOOP_DEV} --typecode=1:EF02
+if [ "$DEVTYPE" = 'disk' ]; then
+    # partition disk
+    # clear existing data
+    sgdisk /dev/${LOOP_DEV} --zap-all
 
-# create 2nd parition
-sgdisk /dev/${LOOP_DEV} --new=2:0:+100M
-sgdisk /dev/${LOOP_DEV} --typecode=2:EF00
+    # create 1st partition
+    sgdisk /dev/${LOOP_DEV} --new=1:0:+1M
+    sgdisk /dev/${LOOP_DEV} --typecode=1:EF02
 
-# create 3nd parition
-# sgdisk /dev/${LOOP_DEV} --new=3:0:1G
-# sgdisk /dev/${LOOP_DEV} --typecode=3:0700
+    # create 2nd parition
+    sgdisk /dev/${LOOP_DEV} --new=2:0:+100M
+    sgdisk /dev/${LOOP_DEV} --typecode=2:EF00
 
-# sfdisk --activate /dev/${LOOP_DEV} 3
+    # create 3nd parition
+    # sgdisk /dev/${LOOP_DEV} --new=3:0:1G
+    # sgdisk /dev/${LOOP_DEV} --typecode=3:0700
 
-# create 3th parition ext
-sgdisk /dev/${LOOP_DEV} --new=3:0:0
-sgdisk /dev/${LOOP_DEV} --typecode=3:8300
+    # create 3th parition ext
+    sgdisk /dev/${LOOP_DEV} --new=3:0:0
+    sgdisk /dev/${LOOP_DEV} --typecode=3:8300
 
-# make hybrid. Assume that if we uses loop device, otherwise it is hard disk ignore
+    # make hybrid. Assume that if we uses loop device, otherwise it is hard disk ignore
 
-if [[ $LOOP_DEV =~ "loop" ]] || [[ $LOOP_DEV =~ "nvme" ]] || [[ $LOOP_DEV =~ "mmcblk" ]]; then
-	HYBRID=yes
-    PART_CHAR="p" # loop device the partition has extra p (parallel devices, not serial one)
-else
-	PART_CHAR=""
+    if [[ $LOOP_DEV =~ "loop" ]] || [[ $LOOP_DEV =~ "nvme" ]] || [[ $LOOP_DEV =~ "mmcblk" ]]; then
+        HYBRID=yes
+        PART_CHAR="p" # loop device the partition has extra p (parallel devices, not serial one)
+    else
+        PART_CHAR=""
+    fi
+
+    FORCE_HYBRID=${FORCE_HYBRID:-no}
+    if [ "$FORCE_HYBRID" = "yes" ]; then HYBRID=yes; fi
+
+    if [[ $HYBRID = "yes" ]]; then
+        sgdisk /dev/${LOOP_DEV} --hybrid=1:2:3
+    else
+        if [ "$FORCE_HYBRID" != "legacy" ]; then
+            echo "Operating on real disk do not make hybrid disk"
+        else
+            echo "Only use legacy boot due to FORCE_HYBRID=$FORCE_HYBRID"
+        fi
+    fi
+
+    # refresh partition table in kernel memory
+    partprobe /dev/${LOOP_DEV}
+
+    # review partition tables
+
+    # using gdisk
+    gdisk -l /dev/${LOOP_DEV}
+
+    # using parted
+    parted -a optimal -s /dev/${LOOP_DEV} print
+
+    # create and mount file-system
+    sleep 5 # hit error device not exists even gdisk list above saw it
+    yes | mkdosfs -F 32 -I -n "BOOTDISK" /dev/${LOOP_DEV}${PART_CHAR}2
+    if [ "$?" != "0" ]; then echo "ERROR mkdosfs -F 32 -I -n \"BOOTDISK\" /dev/${LOOP_DEV}${PART_CHAR}2 . Aborting..."; exit 1; fi
+    #yes | mkdosfs -F 32 -I -n "BOOTDISK" /dev/${LOOP_DEV}${PART_CHAR}3
+    yes | $MKFS -f /dev/${LOOP_DEV}${PART_CHAR}3
+    if [ "$?" != "0" ]; then echo "ERROR $MKFS -f /dev/${LOOP_DEV}${PART_CHAR}3 . Aborting..."; exit 1; fi
+
 fi
 
-FORCE_HYBRID=${FORCE_HYBRID:-no}
-if [ "$FORCE_HYBRID" = "yes" ]; then HYBRID=yes; fi
+mkdir /mnt/root -p
 
-if [[ $HYBRID = "yes" ]]; then
-    sgdisk /dev/${LOOP_DEV} --hybrid=1:2:3
+if [ "$DEVTYPE" = "disk" ]; then
+    TARGET_DEVICE=/dev/${LOOP_DEV}${PART_CHAR}3
 else
-    if [ "$FORCE_HYBRID" != "legacy" ]; then
-        echo "Operating on real disk do not make hybrid disk"
+    TARGET_DEVICE=/dev/${LOOP_DEV}
+fi
+if [ "$FORCE_HYBRID" != "legacy" ]; then # EFI enabled
+    if [ "$DEVTYPE" = "disk" ]; then
+        TARGET_EFI_PART=/dev/${LOOP_DEV}${PART_CHAR}2
     else
-        echo "Only use legacy boot due to FORCE_HYBRID=$FORCE_HYBRID"
+        TARGET_EFI_PART_NO=$(gdisk -l /dev/${DISK_DEVICE} | grep 'EF00' | awk '{print $1}')
+        if [ -z "$TARGET_EFI_PART_NO" ]; then
+            echo "ERROR Could not detect the EFI partition on device ${LOOP_DEV}"
+            exit 1
+        fi
+        TARGET_DEVICE=/dev/${LOOP_DEV}
+        TARGET_EFI_PART=/dev/${DISK_DEVICE}${TARGET_EFI_PART_NO}
     fi
 fi
+    echo "*** ALL DATA ON $TARGET_DEVICE as I am going to create a file system on $TARGET_DEVICE ***"
+    echo "Command MKFS: $MKFS"
+    echo "Type yes to continue"
+    read _confirm
+    if [ "$_confirm" != "yes" ]; then
+        echo "Aborted!"
+        exit 1
+    fi
+    $MKFS $TARGET_DEVICE
 
-# refresh partition table in kernel memory
-partprobe /dev/${LOOP_DEV}
-
-# review partition tables
-# using fdisk
-fdisk -l /dev/${LOOP_DEV}
-
-# using gdisk
-gdisk -l /dev/${LOOP_DEV}
-
-# using parted
-parted -a optimal -s /dev/${LOOP_DEV} print
-
-
-# create and mount file-system
-yes | mkdosfs -F 32 -I -n "BOOTDISK" /dev/${LOOP_DEV}${PART_CHAR}2
-if [ "$?" != "0" ]; then echo "ERROR mkdosfs -F 32 -I -n \"BOOTDISK\" /dev/${LOOP_DEV}${PART_CHAR}2 . Aborting..."; exit 1; fi
-#yes | mkdosfs -F 32 -I -n "BOOTDISK" /dev/${LOOP_DEV}${PART_CHAR}3
-MKFS=${MKFS:-mkfs.btrfs}
-yes | $MKFS -f /dev/${LOOP_DEV}${PART_CHAR}3
-if [ "$?" != "0" ]; then echo "ERROR $MKFS -f /dev/${LOOP_DEV}${PART_CHAR}3 . Aborting..."; exit 1; fi
-
-if [ -d /mnt/root/boot ]; then
-    echo "/mnt/root exist, aborting"
-    exit 1
-else
-    mkdir /mnt/root
-fi
+mount $TARGET_DEVICE /mnt/root
+if [ "$?" != "0" ]; then echo "ERROR mounting $TARGET_DEVICE /mnt/root. Aborting..."; exit 1; fi
 
 if [ "$FORCE_HYBRID" != "legacy" ]; then
-
-    mount /dev/${LOOP_DEV}${PART_CHAR}3 /mnt/root
-    if [ "$?" != "0" ]; then echo "ERROR mounting /dev/${LOOP_DEV}${PART_CHAR}3 /mnt/root. Aborting..."; exit 1; fi
-
     mkdir -p /mnt/root/boot/efi
-    mount /dev/${LOOP_DEV}${PART_CHAR}2 /mnt/root/boot/efi
-
-    grub-install --target=x86_64-efi --efi-directory=/mnt/root/boot/efi --boot-directory=/mnt/root/boot --removable --recheck --uefi-secure-boot
+    mount $TARGET_EFI_PART /mnt/root/boot/efi
+    if [ "$IS_USB" = "true" ]; then GRUB_OPT="--removable"; else GRUB_OPT=""; fi
+    grub-install --target=x86_64-efi --efi-directory=/mnt/root/boot/efi --boot-directory=/mnt/root/boot $GRUB_OPT --recheck --uefi-secure-boot
 fi
 
 if [[ $HYBRID = "yes" ]] || [[ $FORCE_HYBRID = "legacy" ]]; then
-	grub-install --target=i386-pc --boot-directory=/mnt/root/boot /dev/${LOOP_DEV}
+    grub-install --force --target=i386-pc --root-directory=/mnt/root --boot-directory=/mnt/root/boot /dev/${DISK_DEVICE}
 fi
 
-ROOT_PART_UUID=$(grep -oP '(?<=search.fs_uuid )[^\s]+(?= root)' /mnt/root/boot/efi/EFI/BOOT/grub.cfg)
+ROOT_PART_UUID=$(blkid --match-tag UUID $TARGET_DEVICE | grep -oP '(?<=UUID=)[^\s]+' | sed 's/"//g')
 
 if [ -z "$BOOT_FROM" ]; then BOOT_FROM=$(echo $ROOT_PART_UUID | cut -f1 -d-); fi
 
@@ -191,7 +220,12 @@ if [ ! -z "$OS_DIR" ]; then CURRENT_KERNEL_VER="custom"; fi
 OS_DIR=${OS_DIR:-$CURRENT_PORT_DIR/$BOOT_OS}
 echo "Use OS_DIR: '$OS_DIR'"
 
-rsync --exclude '999*' --exclude '*.old' --exclude '*.new' --inplace -avh ${OS_DIR}/ /mnt/root/$BOOT_FROM/${BOOT_OS}/
+mkdir /mnt/root/c-${BOOT_FROM}/${BOOT_OS} -p
+if [[ $MKFS =~ mkfs.btrfs ]]; then
+    chattr +c -R /mnt/root/c-${BOOT_FROM}
+    btrfs property set /mnt/root/c-${BOOT_FROM} compression zstd
+    btrfs property set /mnt/root/c-${BOOT_FROM}/${BOOT_OS} compression zstd
+fi
 
 if [ -z "$CURRENT_KERNEL_VER" ]; then
     CURRENT_KERNEL_VER=$(uname -r)
@@ -201,11 +235,7 @@ else
     cp -a $CUSTOM_PORT_DIR/000-* /mnt/root/$BOOT_FROM/
 fi
 
-mkdir /mnt/root/c-${BOOT_FROM}/${BOOT_OS} -p
-if [[ $MKFS =~ mkfs.btrfs ]]; then
-    chattr +c -R /mnt/root/c-${BOOT_FROM}
-    btrfs property set /mnt/root/c-${BOOT_FROM} compression zstd
-    btrfs property set /mnt/root/c-${BOOT_FROM}/${BOOT_OS} compression zstd
-fi
+rsync --exclude '999*' --exclude '*.old' --exclude '*.new' --inplace -avh ${OS_DIR}/ /mnt/root/$BOOT_FROM/${BOOT_OS}/
+
 sync
 
